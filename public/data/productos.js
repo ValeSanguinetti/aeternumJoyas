e367b3a523f9981a10fe5a9151a37d50ea65cb57
Valeria Sanguinetti <valeriasanguinetti07@gmail.com>
2026-04-14 11:38:33 -0300

    Agregando productos

--- public/data/productos.js
+++ public/data/productos.js
@@ -1,51 +1,40 @@
 const productos = [
-  { nombre: "Anillo cruz infinito", precio: 800,imagen: "img/anillo1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
-  "img/anillo1.jpg",
+  { nombre: "Anillo Flor de Resina", precio: 1938,imagen: "img/anillos/florResina/img1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
+  "img/anillos/florResina/img1.jpeg",
+  "img/anillos/florResina/img2.jpeg",
+  "img/anillos/florResina/img3.jpeg",
 ]},
-  { nombre: "Anillo minimalista", precio: 900, imagen: "img/anillo2.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
-  "img/anillo2.jpg",
-  "img/anillo2-1.jpg",
-  "img/anillo2-2.jpg"
+  { nombre: "Anillo Ovalado", precio: 1889,imagen: "img/anillos/Ovalado/img1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
+  "img/anillos/Ovalado/img1.jpeg",
+  "img/anillos/Ovalado/img2.jpeg",
+  "img/anillos/Ovalado/img3.jpeg",
+]},
+   { nombre: "Anillo Flor de Loto", precio: 1930,imagen: "img/anillos/florLoto/img1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
+  "img/anillos/florLoto/img1.jpeg",
+  "img/anillos/florLoto/img2.jpeg",
+  "img/anillos/florLoto/img3.jpeg",
+]},
+  { nombre: "Anillo Corona", precio: 2145,imagen: "img/anillos/Corona/img1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
+  "img/anillos/Corona/img1.jpeg",
+  "img/anillos/Corona/img2.jpeg",
+  "img/anillos/Corona/img3.jpeg",
+]},
+  { nombre: "Anillo Cruz e Infinito", precio: 1664,imagen: "img/anillos/CruzInfinito/img1.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
+  "img/anillos/CruzInfinito/img1.jpeg",
+  "img/anillos/CruzInfinito/img2.jpeg",
+  "img/anillos/CruzInfinito/img3.jpeg",
+]},
+  { nombre: "Pulsera pop corn", precio: 2932, imagen: "img/Pulseras/popcorn/img1.jpeg", categoria: "Pulseras", tipo: "Plata 925", imagenes: [
+  "img/Pulseras/popcorn/img1.jpeg",
+  "img/Pulseras/popcorn/img2.jpeg",
+  "img/Pulseras/popcorn/img3.jpeg",
 ] },
-  { nombre: "Anillo", precio: 700, imagen: "img/anillo3.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
-  "img/anillo3.jpg",
-  "img/anillo3-1.jpg",
-  "img/anillo3-2.jpg"
-] },
-  { nombre: "Anillo flor de loto", precio: 950, imagen: "img/anillo4.jpeg", categoria: "Anillos", tipo: "Plata 925", imagenes: [
-  "img/anillo4.jpg",
-  "img/anillo4-1.jpg",
-  "img/anillo4-2.jpg"
-] },
-  { nombre: "Caravana de nudos", precio: 1200, imagen: "img/aros1.jpeg", categoria: "Aretes", tipo: "Plata 925", imagenes: [
-  "img/aros1.jpg",
-  "img/aros1-1.jpg",
-  "img/aros1-2.jpg"
-] },
-  { nombre: "Caravana punto de luz", precio: 1200, imagen: "img/aros2.jpeg", categoria: "Aretes", tipo: "Plata 925", imagenes: [
-  "img/aros2.jpg",
-  "img/aros2-1.jpg",
-  "img/aros2-2.jpg"
-] },
-  { nombre: "Aros con Corazones", precio: 900, imagen: "img/aros3.jpeg", categoria: "Aretes", tipo: "Plata 925", imagenes: [
-  "img/aros3.jpg",
-  "img/aros3-1.jpg",
-  "img/aros3-2.jpg"
-] },
-  { nombre: "Aros huggie con circonias", precio: 1100, imagen: "img/aros4.jpeg", categoria: "Aretes", tipo: "Plata 925", imagenes: [
-  "img/aros4.jpg",
-  "img/aros4-1.jpg",
-  "img/aros4-2.jpg"
-] },
-  { nombre: "Pulsera pop corn", precio: 1200, imagen: "img/pulsera1.jpeg", categoria: "Pulseras", tipo: "Plata 925", imagenes: [
-  "img/pulsera1.jpg",
-  "img/pulsera1-1.jpg",
-  "img/pulsera1-2.jpg"
-] },
-  { nombre: "Pulsera triple corazon", precio: 1150, imagen: "img/pulsera2.jpeg", categoria: "Pulseras", tipo: "Plata 925", imagenes: [
-  "img/pulsera2.jpg",
-  "img/pulsera2-1.jpg",
-  "img/pulsera2-2.jpg"
+  { nombre: "Pulsera de corazones", precio: 1591, imagen: "img/Pulseras/pulseraCorazones/img1.jpeg", categoria: "Pulseras", tipo: "Plata 925", imagenes: [
+  "img/Pulseras/pulseraCorazones/img1.jpeg",
+  "img/Pulseras/pulseraCorazones/img2.jpeg",
+  "img/Pulseras/pulseraCorazones/img3.jpeg",
+  "img/Pulseras/pulseraCorazones/img4.jpeg",
+  "img/Pulseras/pulseraCorazones/img5.jpeg",
 ] },
   { nombre: "Pulsera ojo turco", precio: 2433, imagen: "img/Pulseras/pulseraOjoTurco/img1.jpeg", categoria: "Pulseras",tipo: "Plata 925", imagenes: [
   "img/Pulseras/pulseraOjoTurco/img1.jpeg",
